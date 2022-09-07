3a6337a6a3d1d5bb87816c506c062a22727687d1
Money Trees <matveyshakur@gmail.com>
2022-09-07 12:28:52 +0500

    feat: fib numbers finder

---
+++ Fibonacci.js
@@ -0,0 +1,12 @@
+function nthFibo(n) {
+    const array = new Array(n + 1);
+    array[0] = 0;
+    array[1] = 1;
+
+    for (let i = 2; i <= n; i++){
+        array[i] = array[i-1] + array[i-2];
+    }
+    return array[n-1];
+}
+
+console.log(nthFibo(10));
